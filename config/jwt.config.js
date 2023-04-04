1bfed1579336b7df37e57bd09730dff8ca34fcfa
nayra <guerreironayra@gmail.com>
2023-04-04 19:21:33 -0300

    alteração de rota

--- config/jwt.config.js
+++ config/jwt.config.js
@@ -4,7 +4,7 @@ export function generateToken(user) {
   const { _id, name, email, role } = user;
 
   const signature = process.env.TOKEN_SIGN_SECRET;
-  const expiration = "12h";
+  const expiration = "72h";
 
   return jwt.sign({ _id, name, email, role }, signature, {
     expiresIn: expiration,
